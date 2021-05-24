d361850635cf7392dae24fbeb953a92f2a66fdc9
kchavan <kchavan@csod.com>
2021-05-24 19:08:35 +0530

    Fixing heroku deployment issue

--- src/main/java/cowin/slots/CheckSlotController.java
+++ src/main/java/cowin/slots/CheckSlotController.java
@@ -1,31 +1,33 @@
 package cowin.slots;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 
 
 @RestController
 public class CheckSlotController {
 
-	@RequestMapping(method = RequestMethod.GET,path = "/checkslot")
-	
-	public String sayHi(@RequestParam("pincode") String pincode, @RequestParam("date") String date) {
-		
+	@RequestMapping( "/checkslot")
+	public ModelAndView sayHi(@RequestParam("pincode") String pincode, @RequestParam("date") String date) {
+
 		if(pincode != null && pincode != "" && date != null && date != "" ) {
-			
-			
+
 			VaccineService vaccineService = new VaccineService();
 			String data = vaccineService.isAvailable( pincode, date);
-			
-			/*
-			 * ModelAndView mv= new ModelAndView(); mv.addObject("respdata", data);
-			 * mv.setViewName("checkslot.jsp");
-			 */
-			return data;
+
+			ModelAndView mv= new ModelAndView(); 
+			mv.addObject("respdata", data);
+			mv.setViewName("checkslot");
+
+			System.out.println("before return");
+
+			return mv;
 		}
-		
+
 		return null;
 	}
 
